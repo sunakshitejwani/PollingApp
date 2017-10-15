3b11c58c9857a628bffaf03ba901a8d5b08f8326
sunakshi96 <sunakshitejwani@gmail.com>
2017-10-15 22:44:14 +0530

    write more views

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -5,3 +5,12 @@ def index(request):
 	return HttpResponse("Hello World. You are at the poll index.")
 
 # Create your views here.
+def detail(request,question_id):
+	return HttpResponse("You are looking at question %s."% question_id)
+
+def results(request,question_id):
+	response = "You're looking at the results of question %s."
+	return HttpResponse(response % question_id)
+
+def  vote(request,question_id):
+	return HttpResponse("You're voting on question %s" % question_id)
