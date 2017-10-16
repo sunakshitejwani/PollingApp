d7f3df39e630f80164b4b0b1c5a8dd3da263e167
sunakshi96 <sunakshitejwani@gmail.com>
2017-10-16 21:33:57 +0530

    add shortcut get_object_or_404 and use template system

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from django.http import HttpResponse
 from . models import Question
@@ -10,10 +10,7 @@ def index(request):
 	return render(request,'polls/index.html',context)
 # Create your views here.
 def detail(request,question_id):
-	try:
-		question: Question.objects.get(pk=question_id)
-	except Question.DoesNotExist:
-		raise Http404("Question doesnot exist!")
+	question = get_object_or_404(Question,pk=question_id)
 	return render(request,'polls/detail.html',{'question':question})
 
 def results(request,question_id):
