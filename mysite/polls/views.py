f89f05641f2cb7cd3653af46cc5076a4898753f8
sunakshi96 <sunakshitejwani@gmail.com>
2017-10-16 20:23:53 +0530

    add shortcut of render

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -5,12 +5,8 @@ from django.template import loader
 
 def index(request):
 	latest_question_list = Question.objects.order_by('-pub_date')[:5]
-	template = loader.get_template('polls/index.html')
-	context = {
-		'latest_question_list' : latest_question_list
-	}
-	return HttpResponse(template.render(context,request))
-
+	context = {'latest_question_list':latest_question_list}
+	return render(request,'polls/index.html',context)
 # Create your views here.
 def detail(request,question_id):
 	return HttpResponse("You are looking at question %s."% question_id)
